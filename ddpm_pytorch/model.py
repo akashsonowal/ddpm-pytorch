520f5f02698a2a3a750b8d9153917bce03dc2d00
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-18 13:15:30 +0530

    update

--- ddpm_pytorch/model.py
+++ ddpm_pytorch/model.py
@@ -3,3 +3,13 @@ from typing import Optional, Tuple, Union, List
 
 import torch 
 from torch import nn
+
+class Swish(nn.Module):
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(x)
+
+class TimeEmbedding(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self, t: torch.Tensor):
+        return emb 
