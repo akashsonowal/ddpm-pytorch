aab304c81b24e598504b4d1d4b7969771e3e3060
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-22 09:12:35 +0530

    update

--- train.py
+++ train.py
@@ -1,1 +1,18 @@
 from typing import List 
+
+from torch.utils.data import Dataset
+from ddpm_pytorch.model import UNet
+
+class CelebADataset(Dataset):
+    def __init__(self, image_size: int):
+        super().__init__()
+    
+    def __getitem__(self, index: int):
+        img = Image.open(self._files[index])
+        return self._transform(img)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
