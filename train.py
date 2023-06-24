f823df601b36857e50b9b82767530525c739ad93
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-24 16:19:40 +0530

    update

--- train.py
+++ train.py
@@ -1,8 +1,19 @@
 from typing import List 
 
+from tqdm import tqdm
+
+import torch 
+import torch.utils.data
+import torchvision
+from PIL import Image 
+
 from torch.utils.data import Dataset
 from ddpm_pytorch.model import UNet
 from ddpm_pytorch.ddpm import DenoiseDiffusion
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
 
 class CelebADataset(Dataset):
     def __init__(self, image_size: int):
@@ -12,8 +23,22 @@ class CelebADataset(Dataset):
         img = Image.open(self._files[index])
         return self._transform(img)
 
+def train_one_epoch():
+    for data in tqdm(data_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        loss = diffusion.loss(data)
+        loss.backward()
+        optimizer.step()
+
+def sample():
+    pass 
+
 def main():
-    pass
+    for epoch in tqdm(range(epochs)):
+        train_one_epoch()
+        sample()
+
 
 if __name__ == "__main__":
     main()
