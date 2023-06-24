08130d07948de384fc9bdfdc3c9f946db98fc81a
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-24 11:45:29 +0530

    update

--- ddpm_pytorch/ddpm.py
+++ ddpm_pytorch/ddpm.py
@@ -5,3 +5,6 @@ import torch.nn.functional as F
 import torch.utils.data
 from torch import nn
 
+def gather(consts: torch.Tensor, t: torch.Tensor):
+    c = consts.gather(-1, t)
+    return c.reshape(-1, 1, 1, 1)
