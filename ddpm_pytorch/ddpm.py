8983ba81dc349eaef17aecd11ffcf9e08859d02c
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-24 13:13:32 +0530

    update

--- ddpm_pytorch/ddpm.py
+++ ddpm_pytorch/ddpm.py
@@ -12,4 +12,30 @@ def gather(consts: torch.Tensor, t: torch.Tensor):
 class DenoiseDiffusion:
     def __init__(self, eps_model: nn.Module, n_steps: int, device: torch.device):
         super().__init__()
-        
+        self.eps_model = eps_model
+        self.beta = torch.linspace(0.001, 0.2, n_steps).to(device)
+        self.alpha = 1. - self.beta
+        self.alpha_bar = torch.cumprod(self.alpha, dim=0)
+        self.n_steps = n_steps
+        self.sigma2 = self.beta 
+    
+    def q_xt_x0(self, x0: torch.Tensor, t: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        mean = gather(self.alpha_bar, t) ** 0.5 * x0
+        var = 1 - gather(self.alpha_bar, t)
+        return mean, var 
+
+    def q_sample(self, x0: torch.Tensor, t: torch.Tensor, eps: Optional[torch.Tensor] = None):
+        if eps is None:
+            eps = torch.randn_like(x0)
+
+        mean, var = self.q_xt_x0(x0, t)
+        return mean + (var ** 0.5) * eps
+    
+    def p_sample():
+        pass 
+    
+    def loss():
+        pass 
+
+
+
