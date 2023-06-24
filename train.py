83646baf5a5b0f4ca99003457ea5aacd7e9af970
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-24 18:57:20 +0530

    update

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ import torch.utils.data
 import torchvision
 from PIL import Image 
 
+from pathlib import Path
+
 from torch.utils.data import Dataset
 from ddpm_pytorch.model import UNet
 from ddpm_pytorch.ddpm import DenoiseDiffusion
@@ -14,12 +16,21 @@ from ddpm_pytorch.ddpm import DenoiseDiffusion
 class CelebADataset(Dataset):
     def __init__(self, image_size: int):
         super().__init__()
+        folder = Path("./data") / "celebA"
+        self._files = [p for p in folder.glob(f"**/*.jpg")]
+        self._transform = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(image_size),
+            torchvision.transforms.ToTensor(),
+        ])
+    
+    def __len__(self):
+        return len(self._files)
     
     def __getitem__(self, index: int):
         img = Image.open(self._files[index])
         return self._transform(img)
 
-def train_one_epoch():
+def train_one_epoch(diffusion, optimizer, data_loader, device):
     for data in tqdm(data_loader):
         data = data.to(device)
         optimizer.zero_grad()
@@ -30,12 +41,21 @@ def train_one_epoch():
 def sample():
     pass 
 
+
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    dataset = CelebADataset(image_size=32)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True, pin_memory=True)
+
+    eps_model = UNet(image_channels=3, n_channels=64, ch_mults=[1, 2, 2, 4], is_attn=[False, False, False, True]).to(device)
+    diffusion = DenoiseDiffusion(eps_model, n_steps=1000, device=device)
+
+    optimizer = torch.optim.Adam(eps_model.parameters(), lr=2e-5)
+
     for epoch in tqdm(range(epochs)):
-        train_one_epoch()
+        train_one_epoch(diffusion, optimizer, data_loader, device)
         sample()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     main()
