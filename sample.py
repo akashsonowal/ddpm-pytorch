858dca6522e46ddd95ac174bc32990f4edf51e35
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-24 13:45:33 +0530

    update

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
 import numpy as np
+import torch 
+from matplotlib import pyplot as plt
+class Sampler:
+    def sample(self):
+        pass 
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
