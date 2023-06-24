23417a9ee8ebf21df2db3bc002320d6d971f0d7c
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-24 12:59:40 +0530

    update

--- ddpm_pytorch/ddpm.py
+++ ddpm_pytorch/ddpm.py
@@ -8,3 +8,8 @@ from torch import nn
 def gather(consts: torch.Tensor, t: torch.Tensor):
     c = consts.gather(-1, t)
     return c.reshape(-1, 1, 1, 1)
+
+class DenoiseDiffusion:
+    def __init__(self, eps_model: nn.Module, n_steps: int, device: torch.device):
+        super().__init__()
+        
