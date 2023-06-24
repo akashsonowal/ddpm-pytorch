cb9a34b09b5b8327940de8e945cc073bb1d40622
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-24 13:55:05 +0530

    update

--- sample.py
+++ sample.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch 
 from matplotlib import pyplot as plt
+from torchvision.transforms.functional import to_pil_image, resize 
+
+
 class Sampler:
     def sample(self):
         pass 
