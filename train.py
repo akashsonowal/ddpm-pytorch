a1593c22c5858af89c810027a5b69d3e17ab5a4d
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-26 08:22:44 +0530

    update

--- train.py
+++ train.py
@@ -15,23 +15,6 @@ from ddpm_pytorch.data import MNISTDataset
 from ddpm_pytorch.model import UNet
 from ddpm_pytorch.ddpm import DenoiseDiffusion
 
-# class CelebADataset(Dataset):
-#     def __init__(self, image_size: int):
-#         super().__init__()
-#         folder = Path("./data") / "celebA"
-#         self._files = [p for p in folder.glob(f"**/*.jpg")]
-#         self._transform = torchvision.transforms.Compose([
-#             torchvision.transforms.Resize(image_size),
-#             torchvision.transforms.ToTensor(),
-#         ])
-    
-#     def __len__(self):
-#         return len(self._files)
-    
-#     def __getitem__(self, index: int):
-#         img = Image.open(self._files[index])
-#         return self._transform(img)
-
 def train_one_epoch(diffusion, optimizer, data_loader, device):
     for data in tqdm(data_loader):
         data = data.to(device)
@@ -47,7 +30,6 @@ def sample(diffusion, n_samples, image_channels, image_size, n_steps, device):
         for t_ in range(n_steps):
             t = n_steps - t_- 1
             x = diffusion.p_sample(x, x.new_full((n_samples,), t, dtype=torch.long))
-
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
