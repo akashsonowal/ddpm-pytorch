7d0dca1357f234b6dab328a65d5ff665caf79c72
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-26 07:47:41 +0530

    Update data.py

--- ddpm_pytorch/data.py
+++ ddpm_pytorch/data.py
@@ -1,1 +1,13 @@
 #!usr/bin/python3
+import torchvision
+
+class MNISTDataset(torchvision.datasets.MNIST):
+    def __init__(self, image_size):
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(image_size),
+            torchvision.transforms.ToTensor(),
+        ])
+        super().__init__(root='./data', train=True, download=True, transform=transform)
+    
+    def __getitem__(self, item):
+        return super().__getitem__(item)[0]
