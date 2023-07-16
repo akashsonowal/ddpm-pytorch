f9f38f8e72d72f2175957595a8371a6a9fca913d
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-16 20:51:36 +0530

    update

--- ddpm_pytorch/ddpm.py
+++ ddpm_pytorch/ddpm.py
@@ -15,17 +15,17 @@ class DenoiseDiffusion:
     def __init__(self, eps_model: nn.Module, n_steps: int, device: torch.device):
         super().__init__()
         self.eps_model = eps_model
-        self.beta = torch.linspace(0.001, 0.2, n_steps).to(device) # linear schedule
+        self.beta = torch.linspace(0.001, 0.2, n_steps).to(device)  # linear schedule
         self.alpha = 1.0 - self.beta
         self.alpha_bar = torch.cumprod(self.alpha, dim=0)
         self.n_steps = n_steps
         self.sigma2 = self.beta
 
-    def q_xt_x0(
+    def q_xt_x0(  # forward noise from x0 to all timestamps included in the batch t
         self, x0: torch.Tensor, t: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        mean = gather(self.alpha_bar, t) ** 0.5 * x0
-        var = 1 - gather(self.alpha_bar, t)
+        mean = gather(self.alpha_bar, t) ** 0.5 * x0  # mean has to be broadcasted
+        var = 1 - gather(self.alpha_bar, t)  # var has to be broadcasted
         return mean, var
 
     def q_sample(
@@ -37,11 +37,13 @@ class DenoiseDiffusion:
         mean, var = self.q_xt_x0(x0, t)
         return mean + (var**0.5) * eps
 
-    def p_sample(self, xt: torch.Tensor, t: torch.Tensor):
-        eps_theta = self.eps_model(xt, t)
-        alphs_bar = gather(self.alpha_bar, t)
+    def p_sample(self, xt: torch.Tensor, t: torch.Tensor):  # reverse diffusion
+        eps_theta = self.eps_model(
+            xt, t
+        )  # model predicts at the time stamps present in the batch
+        alpha_bar = gather(self.alpha_bar, t)
         alpha = gather(self.alpha, t)
-        eps_coef = (1 - alpha) / (1 - alphs_bar) ** 0.5
+        eps_coef = (1 - alpha) / (1 - alpha_bar) ** 0.5
         mean = 1 / (alpha**0.5) * (xt - eps_coef * eps_theta)
         var = gather(self.sigma2, t)
         eps = torch.randn(xt.shape, device=xt.device)
@@ -50,7 +52,11 @@ class DenoiseDiffusion:
     def loss(self, x0: torch.Tensor, noise: Optional[torch.Tensor] = None):
         batch_size = x0.shape[0]
         t = torch.randint(
-            0, self.n_steps, (batch_size,), device=x0.device, dtype=torch.long
+            0,
+            self.n_steps,
+            (batch_size,),
+            device=x0.device,
+            dtype=torch.long,  # samples randomly time stamps for the batch
         )
 
         if noise is None:
