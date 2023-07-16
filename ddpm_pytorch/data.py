3c5a14f84573744251bfefb445366f2f810dbbcb
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-16 19:33:24 +0530

    update

--- ddpm_pytorch/data.py
+++ ddpm_pytorch/data.py
@@ -1,13 +1,16 @@
 #!usr/bin/python3
 import torchvision
 
+
 class MNISTDataset(torchvision.datasets.MNIST):
     def __init__(self, image_size):
-        transform = torchvision.transforms.Compose([
-            torchvision.transforms.Resize(image_size),
-            torchvision.transforms.ToTensor(),
-        ])
-        super().__init__(root='./data', train=True, download=True, transform=transform)
-    
+        transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize(image_size),
+                torchvision.transforms.ToTensor(),
+            ]
+        )
+        super().__init__(root="../data", train=True, download=True, transform=transform)
+
     def __getitem__(self, item):
-        return super().__getitem__(item)[0]
+        return super().__getitem__(item)[0]  # get only the image
